561f54c4ec47ee5404918e76762c0a01e7443a3a
fazliddindehkanoff <fazliddindehqonov092@gmail.com>
2023-07-06 15:25:32 +0500

    bug fix'

--- students/urls.py
+++ students/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import home_view, students_view
 
 urlpatterns = [
-    path('home/', home_view, name="home"),
+    path('', home_view, name="home"),
     path('student/<int:pk>', students_view, name="student-detail")
 ]
