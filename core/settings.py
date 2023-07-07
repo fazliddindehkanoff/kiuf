7c2b7addc189ea9c6eed644cb6abed5b5e9845cd
fazliddindehkanoff <fazliddindehqonov092@gmail.com>
2023-07-07 17:07:46 +0500

    bug fix'

--- core/settings.py
+++ core/settings.py
@@ -98,7 +98,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [
-    BASE_DIR / "static"
+    BASE_DIR / "staticfiles"
 ]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
