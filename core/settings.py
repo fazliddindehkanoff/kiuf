e8dc77e433585ca2c96d0d578c58562dc8c56a68
fazliddindehkanoff <fazliddindehqonov092@gmail.com>
2023-07-07 17:36:34 +0500

    bug fix'

--- core/settings.py
+++ core/settings.py
@@ -96,7 +96,7 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
